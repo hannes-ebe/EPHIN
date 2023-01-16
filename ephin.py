2242dc317b3ff1e7570ac0c5a37f4d77e3f81067
ebeling <ebeling@physik.uni-kiel.de>
2023-01-16 21:32:44 +0100

    Added License

--- ephin.py
+++ ephin.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2023, Hannes Ebeling
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 import matplotlib.pyplot as plt
 
